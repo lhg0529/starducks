6fbd14ac637c67cdf4cb50f4884a4a2e038d8a54
lhg0529 <lhg0529@naver.com>
2023-11-23 18:24:09 +0900

    사이드바 메뉴 수정 중

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import './Sidebar.css'; 
+import './Sidebar.css';
 const Sidebar = ({ onLogout }) => {
   const navigate = useNavigate();
 
@@ -22,13 +22,50 @@ const Sidebar = ({ onLogout }) => {
             <li><Link to="/mypage/meeting-room">회의실 예약</Link></li>
           </ul>
         </li>
-        <li><Link to="/e-document">전자결재</Link></li>
+        <li><Link to="/e-document">전자결재</Link>
+          <ul>
+            <li><Link to="/e-document/my">나의 결재</Link></li>
+            <li><Link to="/e-document/create">결재문서 작성</Link></li>
+            <li><Link to="/e-document/inbox">수신함</Link></li>
+            <li><Link to="/e-document/drafts">임시 저장함</Link></li>
+          </ul>
+        </li>
         <li><Link to="/forum">전사 게시판</Link></li>
-        {/* 추가 부서 메뉴 */}
-        <li><Link to="/hr">인사부</Link></li>
-        <li><Link to="/logistics">물류유통부</Link></li>
-        <li><Link to="/general-affairs">총무부</Link></li>
-        <li><Link to="/finance">재무부</Link></li>
+        <li>인사부
+          <ul>
+            <li><Link to="/e-document/my">사원 관리</Link></li>
+            <li><Link to="/e-document/create">급여 관리</Link></li>
+            <li><Link to="/e-document/inbox">근태 관리</Link></li>
+            <li><Link to="/e-document/drafts">휴가 관리</Link></li>
+            <li><Link to="/e-document/inbox">부서 관리</Link></li>
+            <li><Link to="/e-document/drafts">문서 관리</Link></li>
+          </ul>
+        </li>
+        <li>물류유통부
+          <ul>
+            <li>입출고관리
+              <ul>
+                <li><Link to="/logistics/incoming">입고관리</Link></li>
+                <li><Link to="/logistics/outgoing">출고관리</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/logistics/inventory">재고관리</Link></li>
+          </ul>
+        </li>
+        <li>총무부
+          <ul>
+            <li><Link to="/general-affairs/schedule">일정 관리</Link></li>
+            <li><Link to="/general-affairs/notice">게시판 공지 등록</Link></li>
+            <li><Link to="/general-affairs/meeting-room">회의실 관리</Link></li>
+            <li><Link to="/general-affairs/items">품목 관리</Link></li>
+          </ul>
+        </li>
+        <li>재무부
+          <ul>
+            <li><Link to="/finance/branch-info">지점 정보 관리</Link></li>
+            <li><Link to="/finance/partner-info">거래처 정보 관리</Link></li>
+          </ul>
+        </li>
         <li><Link to="/org-chart">조직도</Link></li>
       </ul>
       <button onClick={handleLogout} className="logout-button">로그아웃</button>
